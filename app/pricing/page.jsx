68fd5dd99e7cf3fdabcde83a97e2f7e550af5f48
TifeLaflame <obideyib@gmail.com>
2023-08-01 21:29:08 -0600

    set up files

---
+++ app/pricing/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Pricing = () => {
+  return( 
+    <div className="w-full">
+      <div className="mx-auto mt-11 items-center text-center max-w-4xl">
+        <h2 className="text-4xl bold leading-6">
+          Pricing Standards for Learning Products
+        </h2>
+      </div>
+    </div>
+  )
+};
+
+export default Pricing;
